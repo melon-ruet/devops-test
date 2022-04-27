e172cdb337364d8f444459a74c7bdaad594322b9
Melon <melon.ruet@gmail.com>
2022-04-27 11:15:43 +0600

    ecs iam roles

---
+++ terraform/ecs_iam.tf
@@ -0,0 +1,45 @@
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name               = "${var.prefix}-ecs-task-execution"
+  assume_role_policy = data.aws_iam_policy_document.ecs_task_assume_role.json
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_iam_role" "frontend_task_role" {
+  name               = "${var.prefix}-frontend-task-role"
+  assume_role_policy = data.aws_iam_policy_document.ecs_task_assume_role.json
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_iam_role" "backend_task_role" {
+  name               = "${var.prefix}-backend-task-role"
+  assume_role_policy = data.aws_iam_policy_document.ecs_task_assume_role.json
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_iam_role_policy" "backend_rds_role_policy" {
+  name   = "${var.prefix}-backend-rds-role-policy"
+  role   = aws_iam_role.backend_task_role.id
+  policy = data.aws_iam_policy_document.rds_permission.json
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
