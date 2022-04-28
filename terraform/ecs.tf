742855d4892aaba8b2ee59e37dd84b04d1ca7618
Melon <melon.ruet@gmail.com>
2022-04-28 18:10:26 +0600

    ecs backend service added

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -5,50 +5,20 @@ resource "aws_ecs_cluster" "cluster" {
     name  = "containerInsights"
     value = "enabled"
   }
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
-#resource "aws_ecs_task_definition" "frontend_task" {
-#  family                   = "${var.prefix}-frontend-task"
-#  requires_compatibilities = ["FARGATE"]
-#  network_mode             = "awsvpc"
-#  cpu                      = var.task_definition_cpu
-#  memory                   = var.task_definition_memory
-#  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
-#  task_role_arn            = aws_iam_role.frontend_task_role.arn
-#
-#  ephemeral_storage {
-#    size_in_gib = 200
-#  }
-#
-#  container_definitions = jsonencode([
-#    {
-#      name      = "${var.prefix}-frontend-container"
-#      image     = "${aws_ecr_repository.frontend.repository_url}:${local.frontend_dockerfile_dir_hash}"
-#      cpu       = var.container_cpu
-#      memory    = var.container_memory
-#      essential = true
-#      portMappings = [
-#        {
-#          containerPort = 80
-#          hostPort      = 80
-#        }
-#      ]
-#    }
-#  ])
-#}
-
-resource "aws_ecs_task_definition" "ecs_task" {
-  family                   = "${var.prefix}-task"
+resource "aws_ecs_task_definition" "ecs_frontend_task" {
+  family                   = "${var.prefix}-frontend-task"
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
   cpu                      = var.task_definition_cpu
   memory                   = var.task_definition_memory
   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
   task_role_arn            = aws_iam_role.backend_task_role.arn
-
-  ephemeral_storage {
-    size_in_gib = 200
-  }
 
   container_definitions = jsonencode([
     {
@@ -63,27 +33,90 @@ resource "aws_ecs_task_definition" "ecs_task" {
           hostPort      = 80
         }
       ]
-    },
+    }
+  ])
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_ecs_service" "frontend_service" {
+  name            = "${var.prefix}-frontend-service"
+  cluster         = aws_ecs_cluster.cluster.id
+  task_definition = aws_ecs_task_definition.ecs_frontend_task.arn
+  launch_type = "FARGATE"
+  desired_count   = 1
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.lb_target_group.arn
+    container_name   = "${var.prefix}-frontend-container"
+    container_port   = 80
+  }
+
+  network_configuration {
+    subnets = data.aws_subnets.default.ids
+    assign_public_ip = true
+  }
+
+  depends_on = [aws_alb_listener.app_http]
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_ecs_task_definition" "ecs_backend_task" {
+  family                   = "${var.prefix}-backend-task"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = var.task_definition_cpu
+  memory                   = var.task_definition_memory
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  task_role_arn            = aws_iam_role.backend_task_role.arn
+
+  container_definitions = jsonencode([
     {
       name      = "${var.prefix}-backend-container"
       image     = "${aws_ecr_repository.backend.repository_url}:${local.backend_dockerfile_dir_hash}"
       cpu       = var.container_cpu
       memory    = var.container_memory
       essential = true
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+        }
+      ]
     }
   ])
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
-resource "aws_ecs_service" "frontend_service" {
-  name            = "${var.prefix}-frontend-service"
+resource "aws_ecs_service" "backend_service" {
+  name            = "${var.prefix}-backend-service"
   cluster         = aws_ecs_cluster.cluster.id
-  task_definition = aws_ecs_task_definition.ecs_task.arn
+  task_definition = aws_ecs_task_definition.ecs_backend_task.arn
+  launch_type = "FARGATE"
   desired_count   = 1
-#  iam_role        = aws_iam_role.foo.arn
 
   load_balancer {
     target_group_arn = aws_lb_target_group.lb_target_group.arn
-    container_name   = "${var.prefix}-frontend-container"
+    container_name   = "${var.prefix}-backend-container"
     container_port   = 80
+  }
+
+  network_configuration {
+    subnets = data.aws_subnets.default.ids
+    assign_public_ip = true
+  }
+
+  depends_on = [aws_alb_listener.app_http]
+
+  lifecycle {
+    create_before_destroy = true
   }
 }
